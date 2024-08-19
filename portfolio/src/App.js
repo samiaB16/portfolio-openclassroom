29d2651541141857106916bdfbe0cae7e6913965
sam <sam@iMac-de-sam.local>
2024-08-18 18:35:53 -0700

    creation of the Home component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import Layout from './components/Layout';
+import Home from './components/Home';
 import './App.css';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Layout/>
+      <Home />
     </div>
   );
 }
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+      <>
+        <h1>
+        Samia B.<br/>
+        Développeuse Full Stack<br/>
+        </h1>
+        <p>samia.b@linkedin.com</p>
+        <h2>A propos de moi</h2>
+        <p>Bonjour, je suis Samia, développeuse web junior en reconversion professionnelle avec une solide expérience dans le domaine de l’assurance. 
+        <br />Je suis dotée d’une excellente communication, d’un esprit d’équipe, et d’une rigueur exemplaire. <br />Passionnée par les nouvelles technologies et le développement web, je passe aussi mon temps libre à explorer le monde et à faire du sport. Après tout, coder c’est bien, mais ajouter un peu de kilomètres à mon compteur et quelques endorphines n’a jamais fait de mal à personne !</p>
+      </>
+    );
+  }
+  
+  export default Home;
+  
