1c41ec1e9f709939dda293a98cf24b9097917b63
sam <sam@iMac-de-sam.local>
2024-08-19 17:11:34 -0700

    Update the design of the Formation component

--- portfolio/src/components/Formation.js
+++ portfolio/src/components/Formation.js
@@ -1,44 +1,64 @@
 import React from 'react';
+import { Container, Typography, Box, Paper, Grid } from '@mui/material';
+import { motion } from 'framer-motion';
 import openclassrooms from '../assets/openclassrooms.png';
 import esa from '../assets/esa.png';
 
 const formations = [
-    {
-        title: 'Formation Développeur Web - OpenClassrooms (2023 - 2024)',
-        logo: openclassrooms,
-        details: [
-          'Maîtrise des langages et frameworks : React.js, Node.js, MongoDB',
-          'Optimisation SEO',
-          'Gestion de projet et méthodologies Agile'
-        ]
-      },
-      {
-        title: 'Bac +2 Assurance - Ecole Supérieure des Assurances Paris',
-        logo: esa,
-        details: []
-      }
+  {
+    title: 'Formation Développeur Web - OpenClassrooms (2023 - 2024)',
+    logo: openclassrooms,
+    details: [
+      'Maîtrise des langages et frameworks : React.js, Node.js, MongoDB',
+      'Optimisation SEO',
+      'Gestion de projet et méthodologies Agile'
+    ]
+  },
+  {
+    title: 'Bac +2 Assurance - Ecole Supérieure des Assurances Paris',
+    logo: esa
+  }
 ];
 
-function Formation() {
-    return (
-      <>
-        <h2>Formation</h2>
-        <ul>
-            {formations.map((formation) => (
-                <li key={formation.title}>
-                    <h3>{formation.title}</h3>
-                    <img src={formation.logo} alt={formation.title} style={{ width: '100px' }} />
-                    <ul>
-                        {formation.details.map((detail, index) => (
-                            <li key={index}>{detail}</li>
-                        ))}
-                    </ul>
-                </li>
-            ))}
-        </ul>
-      </>
-    );
-}
+const Formation = () => {  
+  return (
+    <Container>
+      <Box my={4}>
+        <Typography variant="h4" gutterBottom textAlign="center">
+          Formation
+        </Typography>
 
-  
-  export default Formation;
+        <Grid container spacing={4}>
+          {formations.map((formation, index) => (
+            <Grid item xs={12} key={index}>
+              <motion.div
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
+              >
+                <Paper elevation={3} sx={{ padding: 2 }}>
+                <Box display="flex" alignItems="center" mb={2}>
+                    <img src={formation.logo} alt={`${formation.title} logo`} style={{ height: '50px', marginRight: '16px' }} />
+                    <Box>
+                      <Typography variant="h6" component="h3" sx={{ color: '#1976d2' , fontWeight: 'bold' }}>
+                        {formation.title}
+                      </Typography>
+                    </Box>
+                  </Box>
+                  {formation.details && (
+                    <ul>
+                      {formation.details.map((detail, index) => (
+                        <li key={index}>{detail}</li>
+                      ))}
+                    </ul>
+                  )}
+                </Paper>
+              </motion.div>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default Formation;
