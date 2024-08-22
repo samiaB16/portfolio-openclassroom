97665e5d614191709824dd5a68f3ecbacd5172a0
sam <sam@iMac-de-sam.local>
2024-08-21 17:23:10 -0700

    Optimize image loading with lazy loading

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Typography, Box, Paper, Grid } from '@mui/material';
 import { motion } from 'framer-motion';
+import LazyLoad from 'react-lazyload';
 import myImage from '../assets/sam.webp';
 import image1 from '../assets/image1.webp';
 import image2 from '../assets/image2.webp';
@@ -11,7 +12,9 @@ const About = () => {
     <Container maxWidth="lg" sx={{ mt: 4 }}>
       <Box display="flex" alignItems="center" mb={4} sx={{ gap: 2 }}>
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
-          <Box component="img" src={myImage} alt="Samia" sx={{ width: 150, height: 150, borderRadius: '50%' }} />
+          <LazyLoad height={150} offset={100}>
+            <Box component="img" src={myImage} alt="Samia" sx={{ width: 150, height: 150, borderRadius: '50%' }} />
+            </LazyLoad>
         </motion.div>
         <motion.div initial={{ y: -50 }} animate={{ y: 0 }} transition={{ duration: 1, delay: 0.5 }}>
           <Typography
@@ -43,12 +46,15 @@ const About = () => {
           <Grid item xs={12} md={4}>
             <Box display="flex" flexDirection="column" alignItems="center">
               <motion.div initial={{ x: -50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 0.5 }}>
-                <Box component="img" src={image1} alt="Image 1" sx={{ width: 140, height: 150, borderRadius: 2, boxShadow: 3, transform: 'rotate(-5deg)' }} />
+                <LazyLoad height={150} offset={100}>
+                  <Box component="img" src={image1} alt="Image 1" sx={{ width: 140, height: 150, borderRadius: 2, boxShadow: 3, transform: 'rotate(-5deg)' }} />
+                </LazyLoad>
               </motion.div>
               <motion.div initial={{ x: 50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 1, delay: 0.7 }}>
-                <Box component="img" src={image2} alt="Image 2" sx={{ width: 140, height: 150, borderRadius: 2, boxShadow: 3, transform: 'rotate(5deg)', mt: 2 }} />
+                <LazyLoad height={150} offset={100}>
+                  <Box component="img" src={image2} alt="Image 2" sx={{ width: 140, height: 150, borderRadius: 2, boxShadow: 3, transform: 'rotate(5deg)', mt: 2 }} />
+                </LazyLoad>
               </motion.div>
-        
             </Box>
           </Grid>
           <Grid item xs={12} md={8}>
